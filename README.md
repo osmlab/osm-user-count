251ff27ea8e12190a1f40f66fae58ae04afd74aa
Mikel Maron <mikel_maron@yahoo.com>
2016-02-03 12:05:07 -0500

    check if edit is within

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # osm-user-count
 Counts edits per user from QA Tiles
+
+# Usage
+
+* Download and gunzip [OSMQATiles country extract](http://osmlab.github.io/osm-qa-tiles/country.html
+* Change mbtiles path (todo, make args) and bbox 
+* `node index.js > data/mexico_users.js`
+* `node user_details_filtered.js data/mexico_users.js > data/mexico_users_filtered.csv`
--- map.js
+++ map.js
@@ -1,3 +1,6 @@
+var mexico = require('./data/mexico.json');
+var inside = require('turf-inside');
+
 module.exports = function (tileLayers, tile, write, done) {
 
   var users = {};
@@ -5,7 +8,24 @@ module.exports = function (tileLayers, tile, write, done) {
   for (var i = 0; i < tileLayers.osm.osm.length; i++) {
     var ft = tileLayers.osm.osm.feature(i);
 
-    users[ ft.properties._user ] = (users[ ft.properties._user] ? users[ ft.properties._user ] : 0 ) + 1;
+    var geom = ft.toGeoJSON(tile[0], tile[1], tile[2]);
+    var pt = {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point"
+      }
+    }
+
+    if (geom.type == "Point") {
+      pt.geometry.coordinates = geom.geometry.coordinates;
+    } else {
+      pt.geometry.coordinates = geom.geometry.coordinates[0];
+    }
+
+    if (inside(pt, mexico)) {
+      users[ ft.properties._user ] = (users[ ft.properties._user] ? users[ ft.properties._user ] : 0 ) + 1;
+    }
   }
 
   done(null, users);
